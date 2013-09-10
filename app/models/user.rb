a3c4b38d0c3c02b7ae6f40791dbab18b71ef26b4
DeVaris Brown <devaris@devaris.com>
2013-09-10 13:38:30 -0700

    Add user model with Devise

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+ 
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :rememberable, :trackable, :validatable, :omniauth_providers => [:google_oauth2]
+ 
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :provider, :uid, :avatar, :name
+
+  def self.from_omniauth(auth)
+    if user = User.find_by_email(auth.info.email)
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user
+    else
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.username = auth.info.name
+        user.email = auth.info.email
+        user.avatar = auth.info.image
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Zassman::Application.routes.draw do
+  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+
+
   root to: 'pages#home'
   get 'about' => 'pages#about'
 

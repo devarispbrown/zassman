2e61956745dee1371f68cd83e54c3f50433aaf97
DeVaris Brown <devaris@devaris.com>
2013-09-09 23:36:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zassman::Application.config.secret_token = '625c89434d60e6094a4107af47e8b99a5aae2eea58bb01f3b97b0da239c4112c2d1980218783bad94a83fc906b29221413aeb746e45453dabf6ec17c7446b2b5'
